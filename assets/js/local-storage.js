ac95883bb21bd4d5cb08d909f3196681e43cf6c8
unknown <peckham4563@gmail.com>
2022-12-06 19:56:40 -0600

    Added Local Storage for Title

--- assets/js/local-storage.js
+++ assets/js/local-storage.js
@@ -14,15 +14,17 @@ $(document).ready (function () {
     })
 
     var tempArr = JSON.parse(localStorage.getItem("myMovies"))
-    $('#text-area-1').val(tempArr[0]);
-    $('#text-area-2').val(tempArr[1]);
-    $('#text-area-3').val(tempArr[2]);
-    $('#text-area-4').val(tempArr[3]);
-    $('#text-area-5').val(tempArr[4]);
-    $('#text-area-6').val(tempArr[5]);
-    $('#text-area-7').val(tempArr[6]);
-    $('#text-area-8').val(tempArr[7]);
-    $('#text-area-9').val(tempArr[8]);
-    $('#text-area-10').val(tempArr[9]);
+    $('#text-area-list-name').val(tempArr[0]);
+    $('#text-area-1').val(tempArr[1]);
+    $('#text-area-2').val(tempArr[2]);
+    $('#text-area-3').val(tempArr[3]);
+    $('#text-area-4').val(tempArr[4]);
+    $('#text-area-5').val(tempArr[5]);
+    $('#text-area-6').val(tempArr[6]);
+    $('#text-area-7').val(tempArr[7]);
+    $('#text-area-8').val(tempArr[8]);
+    $('#text-area-9').val(tempArr[9]);
+    $('#text-area-10').val(tempArr[10]);
+
 })
 
