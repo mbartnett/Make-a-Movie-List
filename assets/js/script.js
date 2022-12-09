111d3115a160d686bf1dbf245f287d21b4ed1cfc
unknown <peckham4563@gmail.com>
2022-12-08 18:50:53 -0600

    Added semicolons to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 var OMDb_API_key = "1181861f";
-var TMDB_API_key = "151fd4610ef4afc0c93b050f47566952"
-var Rapid_API_key = "2bea83b3a4mshd7df1ef3ac74d82p134909jsn7b4daf15a7c7"
+var TMDB_API_key = "151fd4610ef4afc0c93b050f47566952";
+var Rapid_API_key = "2bea83b3a4mshd7df1ef3ac74d82p134909jsn7b4daf15a7c7";
